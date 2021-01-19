c05d319dd75fb9b5e7a5c40c7d26b68cd7409c6b
pengliang <624151713@qq.com>
2021-01-19 22:47:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Promise
+
+A Promise polyfill with Promises/A+
+
+执行测试：
+
+```
+npm run test
+```
---
+++ src/test.js
@@ -0,0 +1,13 @@
+const MyPromise = require("./MyPromise");
+
+MyPromise.deferred = function () {
+  var result = {};
+  result.promise = new MyPromise(function (resolve, reject) {
+    result.resolve = resolve;
+    result.reject = reject;
+  });
+
+  return result;
+};
+
+module.exports = MyPromise;
